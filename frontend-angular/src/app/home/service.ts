ef08904d1d78c268e850ca72a03ca756748645fc
Samuel-Devx <samuelldeev@gmail.com>
2026-05-25 23:00:28 -0300

    (New) Adcionando pesquisa

--- frontend-angular/src/app/home/service.ts
+++ frontend-angular/src/app/home/service.ts
@@ -1,16 +1,27 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Products } from './products';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Service {
     private http = inject(HttpClient);
+    private apiUrl = 'http://localhost:4040/api/product';
 
    getProducts() {
        return this.http.get<Products[]>('http://localhost:4040/api/product');
     }
+   
+ findByName(name: string): Observable<Products[]> {
 
-    
+  const params = new HttpParams()
+    .set('name', name);
+
+  return this.http.get<Products[]>(
+    'http://localhost:4040/api/product/name',
+    { params }
+  );
+}
 }
--- frontend-angular/src/app/shared/seach/seach.ts
+++ frontend-angular/src/app/shared/seach/seach.ts
@@ -1,41 +1,81 @@
-import { Component } from '@angular/core';
-import { Header } from 'primeng/api';
+import { Component, OnInit } from '@angular/core';
+
 import { TableModule } from 'primeng/table';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+
+import { debounceTime, distinctUntilChanged } from 'rxjs';
+
+import { Service } from '../../home/service';
+import { Products } from '../../home/products';
+
 @Component({
   selector: 'app-seach',
+  standalone: true,
+
   imports: [
     TableModule,
     IconFieldModule,
     InputIconModule,
     InputTextModule,
-    ButtonModule
+    ButtonModule,
+    ReactiveFormsModule
   ],
+
   templateUrl: './seach.html',
   styleUrl: './seach.css',
 })
-export class Seach {
-   products = [
-  { id: 1, name: 'Mouse Gamer', price: 120, account: 12, sku: 'ASD1233' },
-  { id: 2, name: 'Teclado Mecânico', price: 350, account: 15, sku: 'QWE4567' },
-  { id: 3, name: 'Monitor 24"', price: 899, account: 18, sku: 'ZXC7890' },
-  { id: 4, name: 'Notebook Dell', price: 4200, account: 20, sku: 'RTY1122' },
-  { id: 5, name: 'Headset HyperX', price: 280, account: 11, sku: 'FGH3344' },
-  { id: 6, name: 'SSD 1TB', price: 430, account: 14, sku: 'JKL5566' },
-  { id: 7, name: 'RTX 4060', price: 2800, account: 25, sku: 'BNM7788' },
-  { id: 8, name: 'Fonte 750W', price: 470, account: 19, sku: 'UIO9900' },
-  { id: 9, name: 'Gabinete RGB', price: 380, account: 13, sku: 'PAS2233' },
-  { id: 10, name: 'Controle PS5', price: 450, account: 16, sku: 'LKJ4455' }
-];
 
-cols = [
-  { field: 'id', header: 'ID' },
-  { field: 'sku', header: 'SKU' },
-  { field: 'account', header: 'Account' },
-  { field: 'name', header: 'Produto' },
-  { field: 'price', header: 'Preço' }
-];
+export class Seach implements OnInit {
+
+  products: Products[] = [];
+
+  searchControl = new FormControl('');
+
+  constructor(private service: Service) {}
+
+  ngOnInit(): void {
+
+    this.searchControl.valueChanges
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged()
+      )
+      .subscribe((value) => {
+
+        if (value && value.trim() !== '') {
+
+          this.service.findByName(value)
+            .subscribe({
+
+              next: (response) => {
+                this.products = response;
+              },
+
+              error: (err) => {
+                console.error(err);
+              }
+
+            });
+
+        } else {
+
+          this.products = [];
+
+        }
+
+      });
+  }
+
+  cols = [
+    { field: 'id', header: 'ID' },
+    { field: 'sku', header: 'SKU' },
+    { field: 'account', header: 'Account' },
+    { field: 'name', header: 'Produto' },
+    { field: 'price', header: 'Preço' }
+  ];
 }
